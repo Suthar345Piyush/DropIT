64c0d7598401e6a674345e9d096606140769a8d5
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-05-13 16:17:12 +0530

    working on zod validation

---
+++ schemas/signInSchema.ts
@@ -0,0 +1,16 @@
+import * as z from "zod";
+
+
+export const signInSchema = z.object({
+  identifier : z 
+         .string()
+        .min(1 , {message : "Email is  required"})
+        .email({message : "Please enter a valid email"}),
+
+  password : z
+        .string()
+        .min(1 , {message : "Password is required"})
+        .min(8 , {message : "Password must  be atleast of 8 characters"})
+});
+
+
